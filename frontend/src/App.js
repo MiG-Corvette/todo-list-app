2823d275503aba375525719faf8fbc1f30a812f8
MiG-Corvette <aws.3018735@gmail.com>
2024-07-10 02:27:23 +0900

    frontendVer1-20240710

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TodoPage from './pages/TodoPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<TodoPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
